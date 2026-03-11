1b9d159e83d45435e42e071f6f5a2d2dee8b7a19
Jakub Cywinski <s33189@pjwstk.edu.pl>
2026-03-11 11:35:44 +0100

    Male poprawki do dzialania

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿try
 {
   Console.WriteLine("Podaj pierwszą liczbę:");
-  double a = Convert.toDouble(Console.ReadLine());
+  double a = Convert.ToDouble(Console.ReadLine());
 
   Console.WriteLine("Podaj drugą liczbę:");
-  double b = Convert.toDouble(Console.ReadLine());
+  double b = Convert.ToDouble(Console.ReadLine());
 
-  double avg = StatisticsHelper.Aberage(a, b);
+  double avg = StatisticsHelper.Average(a, b);
   Console.WriteLine($"Średnia wynosi: {avg}");
 }
 catch
 {
-  Console.WriteLine("Błąd! Niepoprawne liczby!")
+  Console.WriteLine("Błąd! Niepoprawne liczby!");
 }
