0df91f50677603b8590c8974e8c2795c7f933345
Jakub Cywiński <s33189@pjwstk.edu.pl>
2026-03-11 09:51:59 +0100

    Dodanie bledu

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
-﻿
-Console.WriteLine("Podaj pierwszą liczbę:");
-double a = Convert.toDouble(Console.ReadLine());
+﻿try
+{
+  Console.WriteLine("Podaj pierwszą liczbę:");
+  double a = Convert.toDouble(Console.ReadLine());
 
-Console.WriteLine("Podaj drugą liczbę:");
-double b = Convert.toDouble(Console.ReadLine());
+  Console.WriteLine("Podaj drugą liczbę:");
+  double b = Convert.toDouble(Console.ReadLine());
 
-double avg = StatisticsHelper.Aberage(a, b);
-Console.WriteLine($"Średnia wynosi: {avg}");
+  double avg = StatisticsHelper.Aberage(a, b);
+  Console.WriteLine($"Średnia wynosi: {avg}");
+}
+catch
+{
+  Console.WriteLine("Błąd! Niepoprawne liczby!")
+}
